af8796452fee985fa8f9c5d6a1888c35630a8fc5
sameerramteke6878 <SAMEER.M.RAMTEKE@GMAIL.COM>
2025-10-15 17:40:23 +0530

    Update products_cleaned.sql

--- transformations/products_cleaned.sql
+++ transformations/products_cleaned.sql
@@ -1,1 +1,13 @@
-
+-- This model cleans and standardizes product data.
+SELECT
+    product_id,
+    -- Capitalize product names and categories properly (Title Case).
+    INITCAP(product_name) AS product_name,
+    INITCAP(category) AS category,
+    -- Mark inactive products.
+    CASE
+        WHEN active_flag = 'N' THEN 'Discontinued Product'
+        ELSE product_name
+    END AS product_status
+FROM
+    products_raw
